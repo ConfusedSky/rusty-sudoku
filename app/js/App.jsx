4f4dddaa4ea0d8f3ee79a19d10592e2923926c5a
Masa Maeda <masamaedae@gmail.com>
2020-08-02 19:06:35 -0700

    Finishes second version of the grid

--- app/js/App.jsx
+++ app/js/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Steps from "./Steps";
-import { get_solution } from "../pkg/index";
+import { get_solution, get_grid } from "../pkg/index";
 import Grid from "./Grid";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <h1 className="App-header-item">Sudoku Solver</h1>
       </header>
       <div className="App-rest">
-        <Grid></Grid>
+        <Grid values={get_grid()}></Grid>
         <Steps solution={get_solution()}>
           <div></div>
         </Steps>
--- app/js/Grid.jsx
+++ app/js/Grid.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import "./Grid.css";
 
-function Grid(props) {
+function Cell(props) {
   return (
-    <div className="Grid">
-      {Array(9)
-        .fill(0)
-        .map((v, i) => (
-          <div className="Grid-row" key={i}>
-            {Array(9)
-              .fill(0)
-              .map((v2, i2) => (
-                <div className="Grid-cell-container" key={i2}>
-                  <div className="Grid-cell">{v2}</div>
-                </div>
-              ))}
-          </div>
-        ))}
+    <div className="Grid-cell-container">
+      <div className="Grid-cell-border"></div>
+      <div className="Grid-cell-inner">
+        <div className="Grid-cell-value">{props.value || <>&nbsp;</>}</div>
+      </div>
+    </div>
+  );
+}
+
+function Grid(props) {
+  const cells = (v) =>
+    v.map((value, index) => <Cell key={index} value={value}></Cell>);
+
+  const rows = props.values.map((value, index) => (
+    <div className="Grid-row" key={index}>
+      {cells(value)}
+    </div>
+  ));
+
+  return (
+    <div className="Grid-container">
+      <div className="Grid">{rows}</div>
     </div>
   );
 }
