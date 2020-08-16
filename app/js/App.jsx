1c2d73c372704bc3b7b0336f340e41ddbc224ad3
Masa Maeda <masa@mortgagecoach.com>
2020-08-16 09:08:30 -0700

    Converts main into a pure component

--- app/js/App.jsx
+++ app/js/App.jsx
@@ -5,23 +5,25 @@ import Steps from "./Steps";
 import { get_solution, get_grid } from "../pkg/index";
 import Grid from "./Grid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-header-item">
-          <img src={logo} className="App-logo" alt="logo" />
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-header-item">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <h1 className="App-header-item">Sudoku Solver</h1>
+        </header>
+        <div className="App-rest">
+          <Grid values={get_grid()}></Grid>
+          <Steps solution={get_solution()}>
+            <div></div>
+          </Steps>
         </div>
-        <h1 className="App-header-item">Sudoku Solver</h1>
-      </header>
-      <div className="App-rest">
-        <Grid values={get_grid()}></Grid>
-        <Steps solution={get_solution()}>
-          <div></div>
-        </Steps>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
